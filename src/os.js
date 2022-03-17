76d76b8e9d82eab5dffb77a8e0edd387770872cf
Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2022-03-17 22:46:37 +0100

    Add Menu Start Layout

--- src/os.js
+++ src/os.js
@@ -9,7 +9,8 @@ import "./font-awesome/css/all_pro.css";
 
 let windowID = 1;
 let maximized = 0;
-var switch_active = 0
+var switch_active = 0;
+var menu_active = 0;
 
 // Not Implemented Function Notify
 function notImplemented() {
@@ -105,6 +106,45 @@ function switch_window() {
 
 }
 
+function startMenu(){
+    console.log(menu_active)
+    if (menu_active == 0){
+        $("#app").append('<div class="menu"></div>')
+        $(".menu").append('<div class="" style="text-align: center;font-size: 15px;">Oner OS Web <i>(Start Menu Preview)</i></div>')    
+        $(".menu").append('<div class="app-layout"></div>')
+        $(".menu").append('<div class="favourite-layout"></div>')
+        $(".menu").append('<div class="search-layout"></div>')
+        $(".menu").append('<div class="usr-actions_layout">')
+        $(".usr-actions_layout").append('<i class="fa-solid fa-gear"></i>')
+        $(".usr-actions_layout").append('<i class="fa-solid fa-folder-blank"></i>')
+        $(".usr-actions_layout").append('<i class="fa-solid fa-right-from-bracket"></i>')
+        $(".usr-actions_layout").append('<i class="fa-solid fa-power-off"></i>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-microchip icon"></i></div>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-router icon"></i></div>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-hard-drive icon"></i></div>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-compact-disc icon"></i></div>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-camera icon"></i></div>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-album icon"></i></div>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-terminal icon"></i></div>')
+        $(".favourite-layout").append('<div class="example-favourite-app"><i class="fa-solid fa-database icon"></i></div>')
+        $(".app-layout").append('<div class="box"><div class="icon"><i class="fa-solid fa-ghost"></i></div><div class="text">App Name</div></div>')
+        $(".app-layout").append('<div class="box"><div class="icon"><i class="fa-solid fa-ghost"></i></div><div class="text">App Name</div></div>')
+        $(".app-layout").append('<div class="box"><div class="icon"><i class="fa-solid fa-ghost"></i></div><div class="text">App Name</div></div>')
+        $(".app-layout").append('<div class="box"><div class="icon"><i class="fa-solid fa-ghost"></i></div><div class="text">App Name</div></div>')
+        $(".app-layout").append('<div class="box"><div class="icon"><i class="fa-solid fa-ghost"></i></div><div class="text">App Name</div></div>')
+        $(".app-layout").append('<div class="box"><div class="icon"><i class="fa-solid fa-ghost"></i></div><div class="text">App Name</div></div>')
+		$(".search-layout").append('<input id="search_menu" type="text" placeholder="Search 🔎" style="text-align: center;background: transparent;border: 0;outline: none;">')
+        menu_active = 1;
+    }
+    else if (menu_active == 1){
+        $(".menu").remove();
+        menu_active = 0;
+    }
+    else{
+        alert("Start menu broken. Please Reset Cookie/Session this page");
+    }
+
+}
 
 function build_taskbar() {
     var styleSpan = "height: 60%;margin-top: -1.4vh;margin-left: 1vw;"
@@ -127,7 +167,8 @@ function build_taskbar() {
     $(".dt").append('<span id="time">00:37</span>')
 
     $('#app-menu').click(function () {
-        notImplemented()
+        //notImplemented()
+        startMenu()
     });
 
     $('#app-switcher').click(function () {
