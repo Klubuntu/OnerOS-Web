25166e4d35538a6cb0bf6a8de7595b0ae1cd62e6
Klubuntu <49614906+Klubuntu@users.noreply.github.com>
2022-09-23 15:00:57 +0200

    Test Window

--- src/os/window/layout.js
+++ src/os/window/layout.js
@@ -3,6 +3,52 @@ import $ from 'jquery';
 let windowID = 1;
 let maximized = 0;
 
+export function testWindow(){
+    interact('.window')
+        .draggable({
+            onmove: window.dragMoveListener
+        })
+        .resizable({
+            preserveAspectRatio: false,
+            edges: { left: true, right: true, bottom: true, top: true }
+        })
+        .on('resizemove', function (event) {
+            var target = event.target,
+                x = (parseFloat(target.getAttribute('data-x')) || 0),
+                y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+            // update the element's style
+            target.style.width  = event.rect.width + 'px';
+            target.style.height = event.rect.height + 'px';
+
+            // translate when resizing from top or left edges
+            x += event.deltaRect.left;
+            y += event.deltaRect.top;
+
+            target.style.webkitTransform = target.style.transform =
+                'translate(' + x + 'px,' + y + 'px)';
+
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+            target.textContent = event.rect.width + '×' + event.rect.height;
+        });
+
+        function dragMoveListener (event) {
+            var target = event.target,
+                // keep the dragged position in the data-x/data-y attributes
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            // translate the element
+            target.style.webkitTransform =
+            target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+            // update the posiion attributes
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        }
+}
 
 export function resizable() {
     interact('.window')
@@ -95,8 +141,7 @@ export function draggable() {
 }
 
 export function build_window() {
-    resizable();
-    draggable();
+    testWindow();
     /*$(function () {
         $(".window").resizable({
             maxHeight: "80%",
